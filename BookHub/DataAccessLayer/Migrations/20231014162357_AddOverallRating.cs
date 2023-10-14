0fa3229fce8c79c312a96a7d396d9ff5892c1e0a
Roman Alexander Mariancik <mariancik@ics.muni.cz>
2023-10-14 19:20:28 +0200

    add migration

---
+++ BookHub/DataAccessLayer/Migrations/20231014162357_AddOverallRating.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOverallRating : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "OverallRating",
+                table: "Books",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OverallRating",
+                table: "Books");
+        }
+    }
+}
