a1811a893dfbca2a54875e732cb8b47e9919e181
Roman Alexander Mariancik <mariancik@ics.muni.cz>
2023-10-18 20:00:43 +0200

    added authorize button

--- BookHub/BookHub/Controllers/AuthorController.cs
+++ BookHub/BookHub/Controllers/AuthorController.cs
@@ -7,7 +7,6 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace BookHub.Controllers
 {
-    // [Authorize]
     [ApiController]
     [Route("api/[controller]")]
     public class AuthorController : ControllerBase
