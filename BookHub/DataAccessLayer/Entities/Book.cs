629224c3a315908ae9efc171e90581ab1048c7c5
monnikareh <492597@mail.muni.cz>
2023-10-29 11:02:58 +0100

    book entity old ratings removed

--- BookHub/DataAccessLayer/Entities/Book.cs
+++ BookHub/DataAccessLayer/Entities/Book.cs
@@ -7,21 +7,17 @@ namespace DataAccessLayer.Entities;
 public class Book : BaseEntity, IModelRelated
 {
     public String Name { get; set; }
-    // public int GenreId { get; set; }
-    // [ForeignKey("GenreId")]
-    // public virtual Genre Genre { get; set; } = null!;
     
     public int PublisherId{ get; set; }
     [ForeignKey("PublisherId")] 
     public virtual Publisher Publisher { get; set; } = null!;
-
-    public virtual ICollection<Rating> Ratings { get; } = new List<Rating>();
         
     public int StockInStorage { get; set; }
     public double Price { get; set; }
     
     public int OverallRating { get; set; }
     
+    public virtual ICollection<Rating> Ratings { get; } = new List<Rating>();
     public virtual ICollection<Order> Orders { get; } = new List<Order>();
     public virtual ICollection<Author> Authors { get; set; } = new List<Author>();
     public virtual ICollection<Genre> Genres { get; set; } = new List<Genre>();
