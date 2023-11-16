7f83dbd28dfbf5d13b35ecce1b1de25a6753f5d0
AlzbetaStrompova <alzbeta.strompova@botx.cloud>
2023-11-16 17:05:53 +0100

    Added books to update and create user

--- BookHub/DataAccessLayer/Entities/Book.cs
+++ BookHub/DataAccessLayer/Entities/Book.cs
@@ -5,20 +5,20 @@ namespace DataAccessLayer.Entities;
 
 public class Book : BaseEntity, IModelRelated
 {
-    public String Name { get; set; }
+    public string Name { get; set; }
     
     public int PublisherId{ get; set; }
     [ForeignKey("PublisherId")] 
-    public virtual Publisher Publisher { get; set; } = null!;
+    public Publisher Publisher { get; set; } = null!;
         
     public int StockInStorage { get; set; }
     public double Price { get; set; }
     
     public int OverallRating { get; set; }
     
-    public virtual ICollection<Rating> Ratings { get; } = new List<Rating>();
-    public virtual ICollection<Order> Orders { get; } = new List<Order>();
-    public virtual ICollection<Author> Authors { get; set; } = new List<Author>();
-    public virtual ICollection<Genre> Genres { get; set; } = new List<Genre>();
-    public virtual ICollection<User> Users { get; } = new List<User>();
+    public ICollection<Rating> Ratings { get; } = new List<Rating>();
+    public ICollection<Order> Orders { get; } = new List<Order>();
+    public ICollection<Author> Authors { get; set; } = new List<Author>();
+    public ICollection<Genre> Genres { get; set; } = new List<Genre>();
+    public ICollection<User> Users { get; } = new List<User>();
 }
