f1bc6bc3a01e7edafb42f48fa074519ef127e565
AlzbetaStrompova <alzbeta.strompova@botx.cloud>
2023-11-23 23:46:39 +0100

    Update Author...

--- BookHub/BusinessLayer/Models/AuthorDetail.cs
+++ BookHub/BusinessLayer/Models/AuthorDetail.cs
@@ -3,6 +3,6 @@ namespace BusinessLayer.Models;
 public class AuthorDetail
 {
     public int Id { get; set; }
-    public string Name { get; set; }
-    public ICollection<ModelRelated> Books { get; set; }
+    public required string Name { get; set; }
+    public ICollection<ModelRelated>? Books { get; set; }
 }
