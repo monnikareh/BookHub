c7f2af00b30d3e016067b238fbd79ed2be5dba1d
AlzbetaStrompova <alzbeta.strompova@botx.cloud>
2023-11-23 23:53:30 +0100

    Update User.. and ModelRelated.cs

--- BookHub/BusinessLayer/Models/ModelRelated.cs
+++ BookHub/BusinessLayer/Models/ModelRelated.cs
@@ -2,6 +2,6 @@ namespace BusinessLayer.Models;
 
 public class ModelRelated
 {
-    public int Id { get; set; }
-    public string Name { get; set; }
+    public int Id { get; init; }
+    public required string Name { get; init; }
 }
