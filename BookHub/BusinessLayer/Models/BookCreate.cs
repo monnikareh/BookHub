19307bb62be53b8d2333cf607497592fc66a3f02
AlzbetaStrompova <alzbeta.strompova@botx.cloud>
2023-11-23 23:46:53 +0100

    Update Book..

--- BookHub/BusinessLayer/Models/BookCreate.cs
+++ BookHub/BusinessLayer/Models/BookCreate.cs
@@ -2,11 +2,11 @@ namespace BusinessLayer.Models;
 
 public class BookCreate
 {
-    public string Name { get; set; }
-    public ICollection<ModelRelated> Genres { get; set; } = new List<ModelRelated>();
-    public ModelRelated Publisher { get; set; }
+    public required string Name { get; set; }
+    public ICollection<ModelRelated>? Genres { get; set; } = new List<ModelRelated>();
+    public required ModelRelated Publisher { get; set; }
     public int StockInStorage { get; set; }
     public int OverallRating { get; set; }
     public decimal Price { get; set; }
-    public ICollection<ModelRelated> Authors { get; set; } = new List<ModelRelated>();
+    public ICollection<ModelRelated>? Authors { get; set; } = new List<ModelRelated>();
 }
