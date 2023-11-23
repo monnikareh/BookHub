7e5fa4c9ecbc2da89b7dac51dce0e2352fe0f33f
AlzbetaStrompova <alzbeta.strompova@botx.cloud>
2023-11-23 23:52:45 +0100

    Update Order..

--- BookHub/BusinessLayer/Models/OrderDetail.cs
+++ BookHub/BusinessLayer/Models/OrderDetail.cs
@@ -3,8 +3,8 @@ namespace BusinessLayer.Models;
 public class OrderDetail
 {
     public int Id { get; set; }
-    public ModelRelated User { get; set; }
+    public required ModelRelated User { get; init; }
     public decimal TotalPrice { get; set; }
     public DateTime Date { get; set; } 
-    public virtual ICollection<ModelRelated> Books { get; set; } = new List<ModelRelated>();
+    public ICollection<ModelRelated> Books { get; set; } = new List<ModelRelated>();
 }
