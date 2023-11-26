5b4cf04f0ce4f3f1e7ddd4fcca1320e648d64863
Roman Alexander Mariancik <mariancik@ics.muni.cz>
2023-11-26 14:07:46 +0100

    feat(tests): rating update

--- BookHub/BusinessLayer.Tests/Services/RatingServiceTests.cs
+++ BookHub/BusinessLayer.Tests/Services/RatingServiceTests.cs
@@ -99,32 +99,36 @@ public class RatingServiceTests
         Assert.Equal(ret.Comment, result.Comment);
     }
 
-    // [Fact]
-    // public async Task UpdateRatingAsyncGetById_ReturnsRating()
-    // {
-    //     // Arrange
-    //     var serviceProvider = _serviceProviderBuilder.Create();
-    //     using var scope = serviceProvider.CreateScope();
-    //     var dbContext = scope.ServiceProvider.GetRequiredService<BookHubDbContext>();
-    //     await MockedDbContext.PrepareDataAsync(dbContext);
-    //     var ratingService = scope.ServiceProvider.GetRequiredService<IRatingService>();
-    //
-    //     var ratingToUpdate = TestData.GetMockedRatings().First();
-    //
-    //     ratingToUpdate.Name = "Updated rating";
-    //
-    //     // Act
-    //     var result = await ratingService.UpdateRatingAsync(ratingToUpdate.Id, new RatingCreate
-    //     {
-    //         Name = ratingToUpdate.Name
-    //     });
-    //     var ret = await ratingService.GetRatingByIdAsync(result.Id);
-    //     // Assert
-    //     Assert.NotNull(result);
-    //     Assert.NotNull(ret);
-    //     Assert.Equal(ret.Name, result.Name);
-    //     Assert.Equal(ret.Id, result.Id);
-    // }
+    [Fact]
+    public async Task UpdateRatingAsyncGetById_ReturnsRating()
+    {
+        // Arrange
+        var serviceProvider = _serviceProviderBuilder.Create();
+        using var scope = serviceProvider.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<BookHubDbContext>();
+        await MockedDbContext.PrepareDataAsync(dbContext);
+        var ratingService = scope.ServiceProvider.GetRequiredService<IRatingService>();
+    
+        var ratingToUpdate = TestData.GetMockedRatings().First();
+    
+        ratingToUpdate.Comment = "Updated rating";
+    
+        // Actk
+        var result = await ratingService.UpdateRatingAsync(ratingToUpdate.Id, new RatingDetail
+        {
+            Id = ratingToUpdate.Id,
+            User = EntityMapper.MapModelToRelated(ratingToUpdate.User),
+            Book = EntityMapper.MapModelToRelated(ratingToUpdate.Book),
+            Value = 100,
+            Comment = ratingToUpdate.Comment,
+        });
+        var ret = await ratingService.GetRatingByIdAsync(result.Id);
+        // Assert
+        Assert.NotNull(result);
+        Assert.NotNull(ret);
+        Assert.Equal(ret.Comment, result.Comment);
+        Assert.Equal(ret.Id, result.Id);
+    }
     //
     // [Fact]
     // public async Task DeleteRatingAsyncGetByIdDeleteAgain_ReturnsRating()
