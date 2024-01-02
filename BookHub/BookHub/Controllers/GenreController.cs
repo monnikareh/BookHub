f2497e407193ae08007b11f7e3c10a8a7ac8fda0
AlzbetaStrompova <alzbeta.strompova@botx.cloud>
2024-01-02 19:44:58 +0100

    Added GenreController.cs

---
+++ BookHub/BookHub/Controllers/GenreController.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using BookHub.Models;
+using BusinessLayer.Models;
+using BusinessLayer.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookHub.Controllers;
+
+public class GenreController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    private readonly IGenreService _genreService;
+    
+    public GenreController(ILogger<HomeController> logger, IGenreService genreService)
+    {
+        _logger = logger;
+        _genreService = genreService;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var genres = await _genreService.GetGenresAsync(null);
+        return View(genres);    
+    }
+    
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
