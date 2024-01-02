bab52b9351d5cfbcb14edaa5e45c64f4209c4f4b
AlzbetaStrompova <alzbeta.strompova@botx.cloud>
2024-01-02 20:06:35 +0100

    Added column Authors to book table

---
+++ BookHub/BookHub/Controllers/BookController.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using BookHub.Models;
+using BusinessLayer.Models;
+using BusinessLayer.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookHub.Controllers;
+
+public class BookController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    private readonly IBookService _bookService;
+    
+    public BookController(ILogger<HomeController> logger, IBookService bookService)
+    {
+        _logger = logger;
+        _bookService = bookService;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var books = await _bookService.GetBooksAsync(null, null, null, null, null, null, null);
+        return View(books);    
+    }
+    
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
