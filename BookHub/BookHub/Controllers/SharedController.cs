a63236bfe55f6adb104a68bc69d024b6d84686dc
Roman Alexander Mariancik <mariancik@ics.muni.cz>
2024-01-03 09:25:55 +0100

    moved privacy to shared

---
+++ BookHub/BookHub/Controllers/SharedController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookHub.Controllers;
+
+[Route("[controller]/[action]")]
+public class SharedController : Controller
+{
+    // GET
+    private readonly ILogger<SharedController> _logger;
+
+    public SharedController(ILogger<SharedController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+}
