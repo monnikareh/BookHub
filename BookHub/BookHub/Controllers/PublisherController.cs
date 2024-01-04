6302d34462e8895b29b8f8d30869079270a4d4b3
AlzbetaStrompova <alzbeta.strompova@botx.cloud>
2024-01-04 18:25:42 +0100

    Added functionality and access rules

--- BookHub/BookHub/Controllers/PublisherController.cs
+++ BookHub/BookHub/Controllers/PublisherController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Diagnostics;
 using BookHub.Models;
+using BusinessLayer.Models;
 using BusinessLayer.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookHub.Controllers;
@@ -23,10 +25,65 @@ public class PublisherController : Controller
         return View(publishers);    
     }
     
-
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+    
+    [Authorize(Roles = "Admin")]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    [Authorize(Roles = "Admin")]
+    public async Task<IActionResult> Create(PublisherCreate model)
+    {
+        if (!ModelState.IsValid) return View(model);
+        await _publisherService.CreatePublisherAsync(model);
+        return RedirectToAction("Index");
+    }
+    
+    [Authorize(Roles = "Admin")]
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> Edit(int id)
+    {
+        var publisher = await _publisherService.GetPublisherByIdAsync(id);
+        return View(new PublisherUpdate
+        {
+            Name = publisher.Name,
+            Books = publisher.Books
+        });
+    }
+
+    [Authorize(Roles = "Admin")]
+    [HttpPost("{id:int}")]
+    public async Task<IActionResult> Edit(int id, PublisherUpdate model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(model);
+        }
+        await _publisherService.UpdatePublisherAsync(id, model);
+        return RedirectToAction("Index");
+    }
+    
+    [Authorize(Roles = "Admin")]
+    public async Task<ActionResult> Delete(int id)
+    {
+        await _publisherService.DeletePublisherAsync(id);
+        return RedirectToAction("Index");
+    }
+    
+    [AllowAnonymous]
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> Detail(int id)
+    {
+        var author = await _publisherService.GetPublisherByIdAsync(id);
+        return View(author);
+    }
+    
 }
