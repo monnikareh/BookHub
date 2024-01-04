d46c777e8309f7d77fdb5edddef423f067239595
Roman Alexander Mariancik <mariancik@ics.muni.cz>
2024-01-04 18:54:28 +0100

    feat(mvc): added books and genres

--- BookHub/BookHub/Controllers/GenreController.cs
+++ BookHub/BookHub/Controllers/GenreController.cs
@@ -1,7 +1,9 @@
 ﻿using System.Diagnostics;
 using BookHub.Models;
+using BusinessLayer.Models;
 using BusinessLayer.Services;
 using DataAccessLayer.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookHub.Controllers;
@@ -30,4 +32,60 @@ public class GenreController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+    
+    [Authorize(Roles = "Admin")]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    [Authorize(Roles = "Admin")]
+    public async Task<IActionResult> Create(GenreCreate model)
+    {
+        if (!ModelState.IsValid) return View(model);
+        await _genreService.CreateGenreAsync(model);
+        return RedirectToAction("Index");
+    }
+
+    
+    [Authorize(Roles = "Admin")]
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> Edit(int id)
+    {
+        var genre = await _genreService.GetGenreByIdAsync(id);
+        return View(new GenreCreate()
+        {
+            Name = genre.Name,
+        });
+    }
+
+    [Authorize(Roles = "Admin")]
+    [HttpPost("{id:int}")]
+    public async Task<IActionResult> Edit(int id, GenreCreate model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(model);
+        }
+        await _genreService.UpdateGenreAsync(id, model);
+        return RedirectToAction("Index");
+    }
+    
+    [Authorize(Roles = "Admin")]
+    public async Task<ActionResult> Delete(int id)
+    {
+        await _genreService.DeleteGenreAsync(id);
+        return RedirectToAction("Index");
+    }
+    
+    [AllowAnonymous]
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> Detail(int id)
+    {
+        var genre = await _genreService.GetGenreByIdAsync(id);
+        return View(genre);
+    }
+    
 }
