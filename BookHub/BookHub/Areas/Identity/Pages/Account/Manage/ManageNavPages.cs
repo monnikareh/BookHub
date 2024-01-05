fcde16007d05006f863b6839523b067f6f8bae77
Roman Alexander Mariancik <mariancik@ics.muni.cz>
2024-01-05 14:44:46 +0100

    feat(orders in user): prepared for links

--- BookHub/BookHub/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
+++ BookHub/BookHub/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
@@ -61,6 +61,8 @@ namespace  BookHub.Areas.Identity.Pages.Account.Manage
         /// </summary>
         public static string TwoFactorAuthentication => "TwoFactorAuthentication";
 
+        public static string Order => "Order";
+
         /// <summary>
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
@@ -108,6 +110,9 @@ namespace  BookHub.Areas.Identity.Pages.Account.Manage
         ///     directly from your code. This API may change or be removed in future releases.
         /// </summary>
         public static string TwoFactorAuthenticationNavClass(ViewContext viewContext) => PageNavClass(viewContext, TwoFactorAuthentication);
+        
+        public static string OrderNavClass(ViewContext viewContext) => PageNavClass(viewContext, Order);
+
 
         /// <summary>
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
