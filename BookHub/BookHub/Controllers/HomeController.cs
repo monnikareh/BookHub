677339145016a9947b87d68f38d9ca7c8c9bf382
AlzbetaStrompova <alzbeta.strompova@botx.cloud>
2024-01-07 16:35:23 +0100

    Added search to HomeController.cs

--- BookHub/BookHub/Controllers/HomeController.cs
+++ BookHub/BookHub/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Models;
+using BusinessLayer.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BookHub.Controllers;
 
@@ -6,15 +8,36 @@ namespace BookHub.Controllers;
 public class HomeController : BaseController
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IAuthorService _authorService;
+    private readonly IBookService _bookService;
+    private readonly IGenreService _genreService;
+    private readonly IPublisherService _publisherService;
+    private readonly IRatingService _ratingService;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, IAuthorService authorService, IBookService bookService,
+        IGenreService genreService, IPublisherService publisherService, IRatingService ratingService)
     {
         _logger = logger;
+        _authorService = authorService;
+        _bookService = bookService;
+        _genreService = genreService;
+        _publisherService = publisherService;
+        _ratingService = ratingService;
     }
 
     public IActionResult Index()
     {
         return View();        
+    }
+    
+    public async Task<IActionResult> Search(string? query)
+    {
+        var authors = await _authorService.GetSearchAuthorsAsync(query);
+        var books = await _bookService.GetSearchBooksAsync(query);
+        var genres = await _genreService.GetSearchGenresAsync(query);
+        var publishers = await _publisherService.GetSearchPublishersAsync(query);
+        var ratings = await _ratingService.GetSearchRatingsAsync(query);
+        return View(new SearchType(authors, books, genres, publishers, ratings));        
     }
     
 }
