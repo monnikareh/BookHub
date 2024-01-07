c4a38a16bb93fa99d575911f9bb0f48a7adbbf5d
Roman Alexander Mariancik <mariancik@ics.muni.cz>
2024-01-07 15:50:29 +0100

    fix(orders): user no longer can access orders of other users

--- BookHub/BookHub/Controllers/BaseController.cs
+++ BookHub/BookHub/Controllers/BaseController.cs
@@ -1,6 +1,7 @@
 using System.Diagnostics;
 using BookHub.Models;
 using BusinessLayer.Errors;
+using System.Security.Claims;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookHub.Controllers;
@@ -9,6 +10,28 @@ public class BaseController : Controller
 {
     public IActionResult ErrorView((Error err, string message) error)
     {
-        return View("ErrorView", new ErrorViewModel { Message = error.message, RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        return View("ErrorView",
+            new ErrorViewModel
+                { Message = error.message, RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    protected bool TryParseId(out int id)
+    {
+        var claim = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        if (claim is null)
+        {
+            id = 0;
+            return false;
+        }
+
+        var ret = int.TryParse(claim, out var userId);
+        if (!ret)
+        {
+            id = 0;
+            return false;
+        }
+
+        id = userId;
+        return true;
     }
 }
--- BookHub/BookHub/Controllers/OrderController.cs
+++ BookHub/BookHub/Controllers/OrderController.cs
@@ -1,5 +1,9 @@
+using System.Security.Claims;
+using BookHub.Models;
 using BusinessLayer.Services;
+using DataAccessLayer.Entities;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookHub.Controllers;
@@ -15,43 +19,53 @@ public class OrderController : BaseController
         _logger = logger;
         _orderService = orderService;
     }
-    
+
     [HttpGet("{userId:int}/{bookId:int}")]
     [Authorize]
     public async Task<IActionResult> Append(int userId, int bookId)
     {
         await _orderService.AppendBook(userId, bookId);
-        return RedirectToAction("Detail", "Book", new { id = bookId});
+        return RedirectToAction("Detail", "Book", new { id = bookId });
     }
-    
+
     [HttpGet("{id:int}/")]
     [Authorize]
     public async Task<IActionResult> Pay(int id)
     {
         await _orderService.PayOrderAsync(id);
-        return RedirectToPage("/Account/Manage/Order", new { area = "Identity" });    }
+        return RedirectToPage("/Account/Manage/Order", new { area = "Identity" });
+    }
 
-    
+
     [Authorize]
     [HttpGet("{id:int}")]
     public async Task<IActionResult> Detail(int id)
     {
         var order = await _orderService.GetOrderByIdAsync(id);
         return order.Match(
-            View,
+            o =>
+            {
+                var ret = TryParseId(out var userId);
+                if (ret && o.User.Id == userId) return View(o);
+                return RedirectToPage("/Account/Manage/Order", new { area = "Identity" });
+            },
             ErrorView);
     }
-    
-    
+
+
     [Authorize]
-    [HttpGet("{id:int}")]
-    public async Task<IActionResult> Cart(int id)
+    [HttpGet]
+    public async Task<IActionResult> Cart()
     {
-  
-            var order = await _orderService.GetUnpaidOrder(id);
-            return order.Match(
-                View,
-                _ => View("Empty")
-            );
+        var ret = TryParseId(out var userId);
+        if (!ret)
+        {
+            return View("ErrorView", new ErrorViewModel { Message = "User not found" });
+        }
+        var order = await _orderService.GetUnpaidOrder(userId);
+        return order.Match(
+            View,
+            _ => View("Empty")
+        );
     }
 }
