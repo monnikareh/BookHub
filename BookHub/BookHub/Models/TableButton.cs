9472f06a951abc3a83e2b9527acfcb1386817d96
Roman Alexander Mariancik <mariancik@ics.muni.cz>
2024-01-08 18:28:43 +0100

    fix(partial views): added partial views for buttons

---
+++ BookHub/BookHub/Models/TableButton.cs
@@ -0,0 +1,9 @@
+namespace BookHub.Models;
+
+public class TableButton
+{
+    public int Id { get; set; }
+    public string ControllerName { get; set; } = null!;
+    public bool ShowDetail = true;
+
+}
